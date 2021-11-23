e1ec3ae0f1118333310077f4cfc80acd7cda65a0
tayna costa <taynamoreira1811@gmail.com>
2021-11-23 19:47:44 -0300

    hr-worker implementado banco de dados h2, resourses findall e findbyid

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -1,4 +1,4 @@
-package com.educandoweb.course;
+package com.devsuperior.hrworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
