6abb78fb8a74a84e6079ba16cb5d90168846fe87
tayna costa <taynamoreira1811@gmail.com>
2021-11-24 20:32:09 -0300

    rest templete

--- hr-payroll/src/main/java/com/educandoweb/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/educandoweb/hrpayroll/services/PaymentService.java
@@ -1,13 +1,33 @@
 package com.educandoweb.hrpayroll.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.educandoweb.hrpayroll.entites.Payment;
+import com.educandoweb.hrpayroll.entites.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("tayna", 250.0, days);
+		
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
+	
+	
 }
